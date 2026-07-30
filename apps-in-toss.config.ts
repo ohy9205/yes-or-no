47b9c74f4680b5eb76d083192a2d889c513ff626
오해연 <ohy9205@gngrp.com>
2026-07-30 17:15:24 +0900

    docs: app이름 수정

--- apps-in-toss.config.ts
+++ apps-in-toss.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@apps-in-toss/web-framework/config';
 
 export default defineConfig({
-  appName: 'yes-or-no',
+  appName: 'ddakjeonghae',
 
   brand: {
     primaryColor: '#3182F6'
